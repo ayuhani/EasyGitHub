c40505b2e17fe702dfb89c8b21bf2ae0457a8e40
ayuhani <1132529645@qq.com>
2017-09-22 11:02:07 +0800

    添加图片资源；自定义标题栏

--- index.android.js
+++ index.android.js
@@ -5,28 +5,73 @@
  */
 
 import React, { Component } from 'react';
+import TabNavigator from 'react-native-tab-navigator';
+import { Navigator } from 'react-native-deprecated-custom-components';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
+import Boy from './Boy';
 
 export default class EasyGitHub extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {selectedTab: 'tb_polular'};
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+        <View style={styles.container}>
+          {/**<TabNavigator>
+            <TabNavigator.Item
+                selected={this.state.selectedTab === 'tb_polular'}
+                title="Home"
+                selectedTitleStyle={{color: 'red'}}
+                renderIcon={() => <Image style={styles.bottomImage} source={require('./res/images/ic_polular.png')} />}
+                renderSelectedIcon={() => <Image style={[styles.bottomImage, {tintColor: 'red'}]} source={require('./res/images/ic_polular.png')} />}
+                badgeText="1"
+                onPress={() => this.setState({ selectedTab: 'tb_polular' })}>
+              <View style={styles.page1}></View>
+            </TabNavigator.Item>
+            <TabNavigator.Item
+                selected={this.state.selectedTab === 'tb_trending'}
+                title="Profile"
+                renderIcon={() => <Image style={styles.bottomImage} source={require('./res/images/ic_trending.png')} />}
+                renderSelectedIcon={() => <Image style={[styles.bottomImage, {tintColor: 'red'}]} source={require('./res/images/ic_trending.png')} />}
+                onPress={() => this.setState({ selectedTab: 'tb_trending' })}>
+              <View style={styles.page2}></View>
+            </TabNavigator.Item>
+            <TabNavigator.Item
+                selected={this.state.selectedTab === 'tb_favorite'}
+                title="Favorite"
+                selectedTitleStyle={{color: 'red'}}
+                renderIcon={() => <Image style={styles.bottomImage} source={require('./res/images/ic_polular.png')} />}
+                renderSelectedIcon={() => <Image style={[styles.bottomImage, {tintColor: 'red'}]} source={require('./res/images/ic_polular.png')} />}
+                badgeText="1"
+                onPress={() => this.setState({ selectedTab: 'tb_favorite' })}>
+              <View style={styles.page1}></View>
+            </TabNavigator.Item>
+            <TabNavigator.Item
+                selected={this.state.selectedTab === 'tb_my'}
+                title="My"
+                renderIcon={() => <Image style={styles.bottomImage} source={require('./res/images/ic_trending.png')} />}
+                renderSelectedIcon={() => <Image style={[styles.bottomImage, {tintColor: 'red'}]} source={require('./res/images/ic_trending.png')} />}
+                onPress={() => this.setState({ selectedTab: 'tb_my' })}>
+              <View style={styles.page2}></View>
+            </TabNavigator.Item>
+          </TabNavigator>**/}
+          <Navigator
+              initialRoute={{
+                component: Boy
+              }}
+              renderScene={(route, navigator) => {
+                let Component = route.component;
+                return <Component navigator={navigator} {...route.params}/>;
+              }}
+          />
+        </View>
     );
   }
 }
@@ -34,20 +79,20 @@ export default class EasyGitHub extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  page1: {
+    flex: 1,
+    backgroundColor: 'cyan'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  page2: {
+    flex: 1,
+    backgroundColor: 'green'
   },
+  bottomImage: {
+    width: 24,
+    height: 24
+  }
 });
 
 AppRegistry.registerComponent('EasyGitHub', () => EasyGitHub);
