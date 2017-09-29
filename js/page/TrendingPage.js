aee9c9a51cf9b454e1fc759fdd7eed02c95b4b42
ayuhani <1132529645@qq.com>
2017-09-29 14:28:59 +0800

    实现趋势模块弹窗选择本日、本周、本月功能

--- js/page/TrendingPage.js
+++ js/page/TrendingPage.js
@@ -16,15 +16,23 @@ import DataRepository, {FLAG_STORAGE} from '../expand/dao/DataRepository';
 import TrendingItem from '../common/TrendingItem';
 import LanguageDao, {FLAG_LANGUAGE} from '../expand/dao/LanguageDao';
 import RepositoryDetail from "./RepositoryDetail";
+import Popover from '../common/Popover';
+import TimeSpan from '../model/TimeSpan';
 
 const URL = 'https://github.com/trending/';
+const timeSpanArray = [new TimeSpan('本 日', 'since=daily'),
+  new TimeSpan('本 周', 'since=weekly'),
+  new TimeSpan('本 月', 'since=monthly')];
 
 export default class PopularPage extends Component {
   constructor(props) {
     super(props);
     this.languageDao = new LanguageDao(FLAG_LANGUAGE.flag_language);
     this.state = {
-      languages: []
+      languages: [],
+      isVisible: false,
+      buttonRect: {},
+      timeSpan: timeSpanArray[0]
     }
   }
 
@@ -46,6 +54,48 @@ export default class PopularPage extends Component {
         })
   }
 
+  // 渲染头部的选择视图
+  renderTitleView() {
+    return <View>
+      <TouchableOpacity
+          ref="button"
+          onPress={() => this.showPopover()}>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Text style={{fontSize: 20, color: 'white', marginRight: 5}}>趋势</Text>
+          <Text style={{fontSize: 20, color: 'white'}}>{this.state.timeSpan.showText}</Text>
+          <Image
+              style={{width: 12, height: 12, marginLeft: 5}}
+              source={require('../../res/images/ic_spinner_triangle.png')}/>
+        </View>
+      </TouchableOpacity>
+    </View>
+  }
+
+  // 弹出选择框
+  showPopover() {
+    this.refs.button.measure((ox, oy, width, height, px, py) => {
+      this.setState({
+        isVisible: true,
+        buttonRect: {x: px, y: py, width: width, height: height}
+      });
+    });
+  }
+
+  // 弹窗消失
+  closePopover = () => {
+    this.setState({
+      isVisible: false
+    })
+  }
+
+  // 点击选择框的item
+  onSelectedTimeSpan(timeSpan) {
+    this.setState({
+      timeSpan: timeSpan,
+      isVisible: false
+    })
+  }
+
   render() {
     let content = this.state.languages.length > 0 ? <ScrollableTabView
         renderTabBar={() => <ScrollableTabBar/>}
@@ -57,15 +107,38 @@ export default class PopularPage extends Component {
       {this.state.languages.map((result, i, arr) => {
         let lan = arr[i];
         return lan.checked ?
-            <TrendingTab tabLabel={lan.name} key={i} path={lan.path} {...this.props}></TrendingTab> : null;
+            <TrendingTab
+                tabLabel={lan.name}
+                key={i}
+                path={lan.path}
+                timeSpan={this.state.timeSpan}
+                {...this.props}></TrendingTab> : null;
       })}
     </ScrollableTabView> : null;
+    let timeSpanView = <Popover
+        contentStyle={{backgroundColor: '#343434', opacity: 0.9}}
+        isVisible={this.state.isVisible}
+        fromRect={this.state.buttonRect}
+        placement="bottom"
+        onClose={this.closePopover}>
+      {timeSpanArray.map((result, i, arr) => {
+        return <TouchableOpacity
+            key={i}
+            onPress={() => this.onSelectedTimeSpan(arr[i])}
+        >
+          <Text
+              style={{fontSize: 18, color: 'white', paddingLeft: 10, paddingRight: 10, paddingTop: 5, paddingBottom: 5}}
+          >{arr[i].showText}</Text>
+        </TouchableOpacity>
+      })}
+    </Popover>;
     return (
         <View style={styles.container}>
           <NavigationBar
-              title={'趋势'}
+              titleView={this.renderTitleView()}
           />
           {content}
+          {timeSpanView}
         </View>
     );
   }
@@ -84,18 +157,24 @@ class TrendingTab extends Component {
   }
 
   getUrl(category, timeSpan) {
-    return URL + category + timeSpan;
+    return URL + category + '?' + timeSpan.searchText;
   }
 
   componentDidMount() {
-    this.loadData();
+    this.loadData(this.props.timeSpan, true);
   }
 
-  loadData() {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.timeSpan !== this.props.timeSpan) {
+      this.loadData(nextProps.timeSpan);
+    }
+  }
+
+  loadData(timeSpan) {
     this.setState({
       isLoading: true
     });
-    let url = this.getUrl(this.props.path, '?since=daily');
+    let url = this.getUrl(this.props.path, timeSpan);
     this.dataRepository
         .fetchRepository(url)
         .then(result => {
@@ -158,7 +237,7 @@ class TrendingTab extends Component {
           refreshControl={
             <RefreshControl
                 refreshing={this.state.isLoading}
-                onRefresh={() => this.loadData()}
+                onRefresh={() => this.loadData(this.props.timeSpan)}
                 colors={['#2196f3']}
                 tintColor={'#2196f3'}
                 title='Loading...'
